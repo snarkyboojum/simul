2213a22e7bcb09db2189d9775aa7ffd9d6605664
snarkyboojum <snarkyboojum@gmail.com>
2024-04-23 17:09:28 +1000

    Basic skeleton for window done

--- mvg/src/main.rs
+++ mvg/src/main.rs
@@ -1,5 +1,58 @@
 mod point;
 
-fn main() {
+use winit::{
+    error::OsError, event::{Event, WindowEvent}, event_loop::{ControlFlow, EventLoop}, window::{Window, WindowBuilder}
+};
+
+struct VkApp<'a> {
+    name: &'a str,
+}
+
+impl VkApp<'_> {
+    pub fn new(app_name: &str) -> VkApp {
+
+        return VkApp { name: app_name}; 
+    }
+
+    fn init_window(&self, event_loop: &EventLoop<()>) -> winit::window::Window {
+
+        let window = WindowBuilder::new().build(event_loop).unwrap();
+
+        return window;
+    }
+
+    fn main_loop(&self, event_loop: EventLoop<()>, window: Window) {
+
+        event_loop.set_control_flow(ControlFlow::Poll);
+        let _ = event_loop.run(move |event, elwt| {
+            match event {
+                Event::WindowEvent { 
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    println!("The window close button was pressed; exiting");
+                    elwt.exit();
+                },
+                _ => ()
+            }
+        });
+    }
+
+    fn cleanup(&self) {
+
+    }
+
+}
+
+fn main() -> Result<(), OsError> {
     println!("This package implements multi-view geometry.");
+
+    let app = VkApp::new("Point cloud renderer");
+    let event_loop = EventLoop::new().unwrap();
+
+    let window = app.init_window(&event_loop);
+    app.main_loop(event_loop, window);
+
+    Ok(())
+
 }
