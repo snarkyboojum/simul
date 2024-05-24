d562008252598ef20dee7b80263e32e5b0a7d17a
snarkyboojum <snarkyboojum@gmail.com>
2024-05-25 00:09:01 +1000

    Added simple render pass

--- wgpu-dev/src/lib.rs
+++ wgpu-dev/src/lib.rs
@@ -4,11 +4,7 @@ use std::default;
 use wgpu::Surface;
 
 use winit::{
-    event::{ElementState, Event, KeyEvent, WindowEvent, InnerSizeWriter},
-    event_loop::{ControlFlow, EventLoop},
-    keyboard::{KeyCode, PhysicalKey},
-    window::{WindowBuilder, Window},
-    dpi::PhysicalSize,
+    dpi::{LogicalSize, PhysicalSize}, event::{ElementState, Event, InnerSizeWriter, KeyEvent, WindowEvent}, event_loop::{ControlFlow, EventLoop}, keyboard::{KeyCode, PhysicalKey}, window::{Window, WindowBuilder}
 };
 
 struct Simul<'app> {
@@ -37,14 +33,15 @@ impl<'app> Simul<'app> {
             &wgpu::RequestAdapterOptions {
                 compatible_surface: Some(&surface),
                 force_fallback_adapter: false,
-                power_preference: wgpu::PowerPreference::HighPerformance,
+                // power_preference: wgpu::PowerPreference::HighPerformance(),
+                power_preference: wgpu::PowerPreference::default(),
             }
         ).await.unwrap();
 
         let (device, queue) = adapter.request_device(
             &wgpu::DeviceDescriptor {
                 label: None,
-                required_features: wgpu::Features::default(),
+                required_features: wgpu::Features::empty(),
                 required_limits: wgpu::Limits::default(),
             },
             None
@@ -52,6 +49,7 @@ impl<'app> Simul<'app> {
 
         // println!("Adapter features: {:?}", adapter.features());
         println!("Adapter info: {:?}", adapter.get_info());
+        println!("Size: ({}, {})", size.width, size.height);
 
         let surface_capability = surface.get_capabilities(&adapter);
         let surface_format = surface_capability.formats.iter()
@@ -64,7 +62,8 @@ impl<'app> Simul<'app> {
             format: surface_format,
             width: size.width,
             height: size.height,
-            present_mode: wgpu::PresentMode::AutoVsync,
+            // present_mode: wgpu::PresentMode::AutoVsync,
+            present_mode: surface_capability.present_modes[0],
             desired_maximum_frame_latency: 2,
             alpha_mode: surface_capability.alpha_modes[0],
             view_formats: vec![]
@@ -82,25 +81,82 @@ impl<'app> Simul<'app> {
         }
     }
 
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        self.size = new_size;
-        
-        self.config.width = new_size.width;
-        self.config.height = new_size.height;
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
 
-        // println!("New width and height: ({},{})", self.config.width, self.config.height);
-        self.surface.configure(&self.device, &self.config);
+            println!("New width and height: ({},{})", self.config.width, self.config.height);
+            self.surface.configure(&self.device, &self.config);
+        }
     }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {
+
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+
+        // copy pasta
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+        }
+
+        // submit will accept anything that implements IntoIter
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+
 }
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
-    let event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
+    // setup window
+    let event_loop = EventLoop::new().unwrap();
+
+    let window = WindowBuilder::new()
+                        .with_title("Simul app")
+                        .with_inner_size(LogicalSize::new(800,600))
+                        .build(&event_loop)
+                        .unwrap();
+
+    // create app
     let mut app = Simul::new(&window).await;
 
-    event_loop.set_control_flow(ControlFlow::Poll);
+    // respond to events
+    // event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop.set_control_flow(ControlFlow::Wait);
 
     let _ = event_loop.run(move |event, elwt| {
         match event {
@@ -123,6 +179,16 @@ pub async fn run() {
                 ..
             } => {
                 app.resize(physical_size);
+            },
+            Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } => {
+                app.update();
+                match app.render() {
+                    Ok(_) => {},
+                    Err(e) => eprintln!("{:?}", e),
+                }
             },
             _ => ()
         }
