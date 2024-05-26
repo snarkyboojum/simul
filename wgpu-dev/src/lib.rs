429e064eec27bbadbd0bcd1e6d1ad28b1f7dff40
snarkyboojum <snarkyboojum@gmail.com>
2024-05-26 18:30:43 +1000

    Blend the colour in the fragment shader

--- wgpu-dev/src/lib.rs
+++ wgpu-dev/src/lib.rs
@@ -158,18 +158,22 @@ impl<'app> Simul<'app> {
             label: Some("Render Encoder"),
         });
         {
+            let color = wgpu::Color {
+                r: 0.1,
+                g: 0.2,
+                b: 0.3,
+                a: 1.0,
+            };
+
+            let vertices = 0..3u32;
+
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
                     resolve_target: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        }),
+                        load: wgpu::LoadOp::Clear(color),
                         store: wgpu::StoreOp::Store,
                     },
                 })],
@@ -179,10 +183,9 @@ impl<'app> Simul<'app> {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(0..3, 0..1);
+            render_pass.draw(vertices, 0..1);
         }
 
-        // submit will accept anything that implements IntoIter
         self.queue.submit(std::iter::once(encoder.finish()));
         output.present();
 
--- wgpu-dev/src/shader.wgsl
+++ wgpu-dev/src/shader.wgsl
@@ -2,7 +2,7 @@
 
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
-    @location(0) vert_pos: vec3<f32>,
+    @location(0) vert_pos: vec2<f32>,
 };
 
 @vertex
@@ -11,7 +11,8 @@ fn vs_main(
 ) -> VertexOutput {
     var out: VertexOutput;
     let x = f32(1 - i32(in_vertex_index)) * 0.5;
-    let y = f32(i32(in_vertex_index & 1u) * 2 -1) * 0.5;
+    let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
+    out.vert_pos = vec2<f32>(x,y);
     out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
     return out;
 }
@@ -19,5 +20,6 @@ fn vs_main(
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4<f32>(0.3,0.2,0.1,1.0);
+    return vec4<f32>(in.vert_pos, 0.5, 1.0);
+    // return vec4<f32>(0.3,0.2,0.1,1.0);
 }
