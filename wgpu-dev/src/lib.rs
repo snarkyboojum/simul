aa1ef64ca6712a6b41c7bb4b7a3644108c5ae77e
snarkyboojum <snarkyboojum@gmail.com>
2024-06-27 13:11:25 +1000

    Can now load and use the correct texture from a model

--- wgpu-dev/src/lib.rs
+++ wgpu-dev/src/lib.rs
@@ -894,11 +894,13 @@ impl<'app> Simul<'app> {
             }
 
             // println!("Rendering normal scene");
-            render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
+            // render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
             render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
 
             use model::DrawModel;
-            render_pass.draw_mesh_instanced(&self.obj_model.meshes[0], 0..self.instances.len() as u32);
+            let mesh = &self.obj_model.meshes[0];
+            let material = &self.obj_model.materials[mesh.material];
+            render_pass.draw_mesh_instanced(mesh, material, 0..self.instances.len() as u32, &self.camera_bind_group);
 
             // render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             // render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
